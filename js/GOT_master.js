480acca05d9859d16bdcae98d3c09388bef94a27
Andrés Gallo <andresgallod.i@gmail.com>
2020-07-10 12:51:02 -0400

    got

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -3,17 +3,36 @@
 
   let lightBox = document.querySelector(".lightbox"),
       lbClose = lightBox.querySelector("span"),
+      lbVideo = lightBox.querySelector("video"),
       sigils = document.querySelectorAll(".sigilContainer");
 
 
-  function showLightbox() {
-    lightBox.classList.add('show-Lightbox');
+  function showHideLightbox() {
+    lightBox.classList.toggle('show-lightbox');
+
+  //Try Making the video play as well
+
+  //check the play state of the video.
+  //if it's paused or not playing, that means we just opened the lightBox
+  //and we want the video to play
+  if (lbVideo.paused) {
+    lbVideo.play();
+  } else {
+    //lightbox is open and we want the video to stop and rewind when we close it
+    lbVideo.currentTime = 0;
+    lbVideo.pause();
+
   }
 
-  function hideLightbox() {
-    lightBox.classList.remove('show-lightbox');
-  }
 
-  sigils.forEach(sigil => sigil.addEventListener("click", showLightbox));
-  lbClose.addEventListener("click", hideLightbox);
+  //rewind the video and make it puse at currentTime of 0
+
+
+}
+
+  sigils.forEach(sigil => sigil.addEventListener("click", showHideLightbox));
+  lbClose.addEventListener("click", showHideLightbox);
+
+  //add
+  lbVideo.addEventListener("ended", showHideLightbox);
 })();
