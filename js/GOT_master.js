11fffbf12bfb8cd02cbc4170bdfb3a89bdadb9a2
Andrés Gallo <andresgallod.i@gmail.com>
2020-07-10 11:51:55 -0400

    adding lightbox

---
+++ js/GOT_master.js
@@ -0,0 +1,19 @@
+(()=> {
+  console.log('fired!');
+
+  let lightBox = document.querySelector(".lightbox"),
+      lbClose = lightBox.querySelector("span"),
+      sigils = document.querySelectorAll(".sigilContainer");
+
+
+  function showLightbox() {
+    lightBox.classList.add('show-Lightbox');
+  }
+
+  function hideLightbox() {
+    lightBox.classList.remove('show-lightbox');
+  }
+
+  sigils.forEach(sigil => sigil.addEventListener("click", showLightbox));
+  lbClose.addEventListener("click", hideLightbox);
+})();
